b6bf66d030aa47010b4044131c0663ca3906ea91
Nguyen Phuc Dien <nguyenphucdien2007@gmail.com>
2026-04-29 16:44:17 +0700

    test

---
+++ src/test/java/com/example/MathUtilsTest.java
@@ -0,0 +1,76 @@
+package com.example;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.platform.commons.logging.LoggerFactory;
+
+import java.util.logging.Logger;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class MathUtilsTest {
+    static MathUtils mathUtils;
+    private static final org.slf4j.Logger logger = org.slf4j.LoggerFactory.getLogger(MathUtilsTest.class);
+    @BeforeAll
+    static public void setUp(){
+        mathUtils = new MathUtils();
+        logger.info("=== Bắt đầu test ===");
+    }
+
+    @Test
+    public void testMax1(){
+        assertEquals(5, mathUtils.max(5, 3));
+
+    }
+    @Test
+    public void testMax2(){
+        assertEquals(20, mathUtils.max(10, 20));
+    }
+    @Test
+    public void testMax3(){
+        assertEquals(0, mathUtils.max(0, 0));
+    }
+
+    @Test
+    public void testMax4(){
+        assertEquals(Integer.MAX_VALUE, mathUtils.max(Integer.MAX_VALUE, Integer.MIN_VALUE));
+    }
+    @Test
+    public void testMax5(){
+        assertEquals(Integer.MIN_VALUE, mathUtils.max(Integer.MIN_VALUE, Integer.MIN_VALUE));
+    }
+    @Test
+    public void testMax6(){
+        assertEquals(Integer.MAX_VALUE-1, mathUtils.max(Integer.MAX_VALUE-1, Integer.MAX_VALUE-2));
+    }
+
+
+    @Test
+    public void divide1(){
+        assertEquals(2, mathUtils.divide(10, 5));
+    }
+
+    @Test
+    public void divide2(){
+        assertEquals(-3, mathUtils.divide(9, -3));
+    }
+
+    @Test
+    public void divide3(){
+        assertEquals(0, mathUtils.divide(0, 5));
+    }
+
+    @Test
+    public void divide4(){
+        assertThrows(IllegalArgumentException.class, () -> mathUtils.divide(-5, 0));
+    }
+
+    @AfterAll
+    static public void done(){
+        System.out.println("=== Kết thúc ===");
+    }
+
+
+}
